65ed98c7b913411e2f43d65999749a6099ddc6a9
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2024-03-10 23:51:59 +0700

    added client authentication service using auth0

---
+++ src/auth-client.ts
@@ -0,0 +1,13 @@
+import { Auth0Client } from "@auth0/auth0-spa-js";
+
+const client = new Auth0Client({
+  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+  authorizationParams: {
+    audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+    scope: "openid profile email",
+    redirect_uri: import.meta.env.VITE_LOGIN_REDIRECT_URL,
+  },
+});
+
+export default client;
